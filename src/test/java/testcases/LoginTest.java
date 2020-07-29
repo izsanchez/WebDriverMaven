e6ed3e7b3a9c2663700f21e871fdf660ca454cc7
Isaac Zuniga <isaac.zuniga@itstarksolutions.com>
2020-07-29 10:34:25 -0500

    Initial Commit

---
+++ src/test/java/testcases/LoginTest.java
@@ -0,0 +1,44 @@
+package testcases;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest {
+
+	public static WebDriver driver;
+	
+	@BeforeTest
+	public void setUp() {
+		
+		WebDriverManager.chromedriver().setup();
+		 driver = new ChromeDriver();
+		
+	}
+
+	@Test
+	public void doLogin() {
+		
+		driver.get("http://gmail.com");
+		
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		//driver.findElement(By.linkText("Acceder")).click();
+		System.out.println(driver.getTitle());
+		driver.findElement(By.id("identifierId")).sendKeys("izsanchez@gmail.com");
+	}
+	
+	@AfterTest
+	public void tearDown() {
+		
+		driver.quit();
+	}
+}
